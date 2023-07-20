f82fd3dd5bc92436f855dfe97bf08cd5cd822f15
Vladislav Klepikov <ya.losyash@yandex.ru>
2023-07-20 16:08:56 +0300

    optimized js

--- js.js
+++ js.js
@@ -1,11 +1,14 @@
 /* Это объявление переменной, мы нашли кнопку по тегу */
-let button = document.querySelector('.button-link');
+const button = document.querySelector('.button-link');
+
 let about = document.getElementById("about");
+
 /* Тут на кнопку навешиваем обрабочик, который ждёт клика и тогда запустит логику */
-button.addEventListener ('click',
-function toggleClass(){
-	about.classList.remove("hide");
-	about.classList.add("about");
-	// alert(about.className);  
+button.addEventListener ('click',function toggleClass(){
+	about.classList.toggle('hidden');
 })
 
+// const button = document.querySelector('button.hidden')
+
+// так как класс есть, то он будет убран
+// button.classList.toggle('hidden')
